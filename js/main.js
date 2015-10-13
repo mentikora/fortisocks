1ef4e48f957d080eb1b124a851a4b785d27e7368
Mentikora <notender_boy@ukr.net>
2015-10-13 13:20:38 +0300

    feedback slider, jquery file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,37 @@
-$(function() {
+$(document).ready(function() {
+  // обрабатываем каждый слайдер  
+  $(".slider").each(function () { 
+    var obj = $(this);
+    $(obj).append("<div class='nav'></div>");
+    $(obj).find("li").each(function () {
+      // добавляем блок навигации
+      $(obj).find(".nav").append("<span rel='"+$(this).index()+"'></span>"); 
+      $(this).addClass("slider"+$(this).index());
+    });
+    // делаем активным первый элемент меню
+    $(obj).find("span").first().addClass("on"); 
+  });
+});
+function sliderJS (obj, sl) {
+  // находим блок
+  var ul = $(sl).find("ul"); 
+  // находим любой из элементов блока
+  var bl = $(sl).find("li.slider"+obj); 
+  // ширина объекта
+  var step = $(bl).width(); 
+  // 500 это скорость перемотки
+  $(ul).animate({marginLeft: "-"+step*obj}, 500); 
+}
+$(document).on("click", ".slider .nav span", function() {
+  // находим, в каком блоке был клик  
+  var sl = $(this).closest(".slider"); 
+  // убираем активный элемент
+  $(sl).find("span").removeClass("on"); 
+  // делаем активным текущий
+  $(this).addClass("on"); 
+  // узнаем его номер
+  var obj = $(this).attr("rel"); 
+  // слайдим
+  sliderJS(obj, sl); 
+  return false;
 });
